1d35f94068688b7e7a413fbb94c685071814c53f
Chetupatil24 <chetupatil605@gmail..com>
2026-02-21 18:00:32 +0530

    Update configurations for Railway deployment: Add node engine requirement and DATABASE_URL support

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -4,19 +4,29 @@ const config = require('./database');
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 
-const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    host: dbConfig.host,
-    port: dbConfig.port,
+let sequelize;
+if (dbConfig.use_env_variable && process.env[dbConfig.use_env_variable]) {
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], {
     dialect: dbConfig.dialect,
     logging: dbConfig.logging,
     pool: dbConfig.pool,
     dialectOptions: dbConfig.dialectOptions
-  }
-);
+  });
+} else {
+  sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    {
+      host: dbConfig.host,
+      port: dbConfig.port,
+      dialect: dbConfig.dialect,
+      logging: dbConfig.logging,
+      pool: dbConfig.pool,
+      dialectOptions: dbConfig.dialectOptions
+    }
+  );
+}
 
 // Test connection
 const testConnection = async () => {
