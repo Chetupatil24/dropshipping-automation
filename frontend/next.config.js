874df4c3bf1d9b24134a07cbcc11d2383f35a297
Chetupatil24 <chetupatil605@gmail..com>
2026-02-22 13:34:17 +0530

    chore: setup netlify frontend configuration and connect to cloudflare workers api url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,11 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'via.placeholder.com'],
+    domains: ['res.cloudinary.com'],
   },
-  env: {
-    API_URL: process.env.API_URL || 'http://localhost:5000',
-  }
 }
 
 module.exports = nextConfig
